d0ceea25905827cc45aaab7baf1ebb6e0c9c6147
snaps-hian <hian@snaps.com>
2022-02-10 09:53:05 +0900

    filehandler , formmater

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask
+import logging
+
 
 
 app = Flask(__name__)
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('[%(asctime)s][%(levelname)s|%(filename)s:%(lineno)s] >> %(message)s')
+fileHandler = logging.FileHandler('test.log')
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+logger.setLevel(level=logging.DEBUG)
+
 
 @app.route("/v1/test/hello")
 def test_func():
+    logger.info('request occurred!')
     return 'hello snaps'
 
 
 if __name__ == '__main__':
 
-    app.run('0.0.0.0',port=5000)
+    app.run('0.0.0.0',port=5500)
