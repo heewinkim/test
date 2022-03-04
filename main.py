ff4e11f7186fd92d50083ae4cca1ccfbb8a8ebf9
heewinkim <heewin.kim@gmail.com>
2022-03-04 18:13:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import logging
 application = Flask(__name__)
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter('[%(asctime)s][%(levelname)s|%(filename)s:%(lineno)s] >> %(message)s')
-fileHandler = logging.FileHandler('test.log')
+fileHandler = logging.FileHandler('/home/ubuntu/test.log')
 fileHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
 logger.setLevel(level=logging.DEBUG)
@@ -15,9 +15,9 @@ logger.setLevel(level=logging.DEBUG)
 @application.route("/v1/test/hello")
 def test_func():
     logger.info('request occurred!')
-    return 'hello snaps'
+    return 'hello world'
 
 
 if __name__ == '__main__':
 
-    application.run('0.0.0.0',port=5500)
+    application.run('0.0.0.0',port=5000)
